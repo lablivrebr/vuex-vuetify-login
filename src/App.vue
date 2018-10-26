1474d1810500719e790f10946081219844b3805c
vinnyfs89 <viniciusfesil@gmail.com>
2018-10-26 14:31:07 -0300

    :zap: Commit inicial

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <div id="nav">
+            <router-link to="/">Home</router-link>
+            |
+            <router-link to="/about">About</router-link>
+            <span v-if="isLoggedIn"> | <a @click="logout">Logout</a></span>
+        </div>
+        <router-view/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        computed: {
+            isLoggedIn: function () {
+                return this.$store.getters.isLoggedIn
+            }
+        },
+        methods: {
+            logout: function () {
+                this.$store.dispatch('logout')
+                    .then(() => {
+                        this.$router.push('/login')
+                    })
+            }
+        },
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ src/components/Secure.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>This page is protected by auth</h1>
+  </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                name : "",
+                email : "",
+                password : "",
+                password_confirmation : "",
+                is_admin : null
+            }
+        },
+        methods: {
+            register: function () {
+                let data = {
+                    name: this.name,
+                    email: this.email,
+                    password: this.password,
+                    is_admin: this.is_admin
+                }
+                this.$store.dispatch('register', data)
+                    .then(() => this.$router.push('/'))
+                    .catch(err => console.log(err))
+            }
+        }
+    }
+</script>
